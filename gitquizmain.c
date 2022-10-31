09a140579e692fe71c6deaec4f0f10c67695d484
cneronini <ceronini@uoguelph.ca>
2022-10-31 10:26:38 -0400

    Modified gitQuizMain file in Lab6

---
+++ gitquizmain.c
@@ -0,0 +1,20 @@
+// TO DO: Add an "else" condition after line 12 which prints another message if the user does not enter "y"
+#include <stdio.h>
+
+int main(void)
+{
+    char val;
+    printf("Enter 'y' and press enter if you know how to use Git!\n");
+    scanf(" %c", &val);
+
+    if(val == 'y'){
+        printf("You know how to use Git!");
+    }
+  else{
+      printf("Value must equal to 'y'!");
+  }
+
+    printf("\n");
+
+    return 0;
+}
